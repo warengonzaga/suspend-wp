f16930f5cf61662738e77c425c25f61d04528669
Waren Gonzaga <github@warengonzaga.com>
2021-09-27 14:52:12 +0800

    📦 NEW: Add plugin index file

---
+++ suspend-wp.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Plugin Name: Suspend WP
+* Plugin URI: https://github.com/warengonzaga/suspend-wp
+* Description: A WordPress plugin to suspend WordPress sites automagically. Simple and lightweight, no annoying ads and fancy settings.
+* Version: 1.0.0
+* Author: Waren Gonzaga
+* Author URI: https://warengonzaga.com
+*/
+
+/**
+* WP Update Your Footer
+*/
+
+// prevent direct access
+defined( 'ABSPATH' ) or die( "Restricted Access!" );
