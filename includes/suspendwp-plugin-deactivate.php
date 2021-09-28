4510e501e259922dd8d4aef41c6ca1dd3ea4ad2e
Waren Gonzaga <github@warengonzaga.com>
2021-09-28 16:09:59 +0800

    📦 NEW: split files

---
+++ includes/suspendwp-plugin-deactivate.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Suspend WP Plugin on Deactivation Method
+ *
+ * @package   suspend-wp
+ * @copyright Copyright (c) 2021, Waren Gonzaga
+ * @license   GPL-3.0+
+ */
+
+class SuspendWPDeactivate {
+    public static function deactivate() {
+        // on plugin deactivation method
+    }
+}
